8ac814ffee25e36902c00a102c12f57c4719d50b
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-08 18:26:45 +0100

    Add Automatic Payments

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const Express = require("express")
 const App = Express()
 
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function CreateKey(ApiKey) {
+  return new Promise(async (Res, Rej) => {
+    const Project = await prisma.project.findFirst({
+      where: {
+        api_key: ApiKey
+      }
+    })
+
+    if(Project) {
+      const Response = await prisma.license.create({
+        data: {
+          projectId: Project.id,
+          lifetime: true
+        }
+      })
+      Res(Response.id)
+    } else {
+      Res(false)
+    }
+  }) 
+}
+
+const Reply = CreateKey("aa689c5b-4002-4107-b766-374755509ecc").then((X) => {
+  console.log(X)
+})
+
 App.post("/sellix/:apikey/webhook", (req, res) => {
-  console.log(req.headers["x-sellix-event"])
-  console.log(req)
-  res.send("The Fuck")
+  if(req.headers["x-sellix-event"] === "order:paid:product") {
+    const ApiKey = req.params.apikey
+    CreateKey(ApiKey).then((Key) => {
+      if(Key === false) {
+        return res.status(401)
+      }
+
+      return res.send(Key)
+    })
+  }
+
+  return res.status(301)
 })
 
 const PORT = process.env.PORT || 3000
