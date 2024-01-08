a1e9efc26bfa759ed87d7b3c69563572a1cbe5e4
BartekSadurski <148957368+BartekSadurski@users.noreply.github.com>
2024-01-08 17:40:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Express = require("express")
 const App = Express()
 
-App.get("/sellix/:apikey/webhook", (req, res) => {
+App.post("/sellix/:apikey/webhook", (req, res) => {
   console.log(req.headers["x-sellix-event"])
   console.log(req)
   res.send("The Fuck")
